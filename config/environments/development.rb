d765a7b9bb55ed64074e2b73b73ee926f3f6711e
Ubuntu <ubuntu@ip-172-31-19-224.us-east-2.compute.internal>
2021-07-17 14:10:11 +0000

    static_pages完成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.hosts << "0c682e8373ec49ab9cb60bb762c7cfcd.vfs.cloud9.us-east-2.amazonaws.com"
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
